cdc2b45ebe349de65a31c6506a879eddfe864477
OlegRus <oleg.rus178@gmail.com>
2022-02-19 22:24:23 +0300

    try to fix circle square

--- solution.go
+++ solution.go
@@ -11,8 +11,7 @@ const SidesSquare SideAmount = 4
 func CalcSquare(sideLen float64, sidesNum SideAmount) float64 {
 	switch sidesNum {
 	case 0:
-		r := (sideLen / math.Pi) / 2.0
-		return math.Pow(r, 2.0) * math.Pi
+		return math.Pow(sideLen, 2.0) * math.Pi
 	case 3:
 		return (math.Sqrt(3.0) / 4.0) * math.Pow(sideLen, 2.0)
 	case 4:
